e0779170a2c753c57ef59b276095e5807499e744
chirag <chiragsethi503@gmail.com>
2022-10-07 14:59:17 +0530

    first commit

---
+++ lib/api/api.dart
@@ -0,0 +1,5 @@
+class Api {
+  static const baseUrl = "https://api.themoviedb.org/3/";
+  static const imageBaseUrl = "https://image.tmdb.org/t/p/original/";
+  static const apiKey = "84c5b71a8c6060c657c67f562c249a88";
+}
